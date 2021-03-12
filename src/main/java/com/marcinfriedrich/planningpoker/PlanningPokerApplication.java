c4c2a3753d857fc7a66e34ecc6cf8c8519fd547b
Marcin Friedrich <fredros1996@gmail.com>
2021-03-12 19:47:22 +0100

    fix 1

--- src/main/java/com/marcinfriedrich/planningpoker/PlanningPokerApplication.java
+++ src/main/java/com/marcinfriedrich/planningpoker/PlanningPokerApplication.java
@@ -1,14 +1,13 @@
 package com.marcinfriedrich.planningpoker;
 
-import com.marcinfriedrich.planningpoker.cache.RoomCacheManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlanningPokerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlanningPokerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlanningPokerApplication.class, args);
+    }
 
 }
