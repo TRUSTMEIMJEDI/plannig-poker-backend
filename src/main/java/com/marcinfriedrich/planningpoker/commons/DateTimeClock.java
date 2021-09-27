429d7799b2904eed541edd2066e52cc23bb3cfce
Marcin Friedrich <fredros1996@gmail.com>
2021-09-27 23:25:41 +0200

    change visibility of date time clock class

--- src/main/java/com/marcinfriedrich/planningpoker/commons/DateTimeClock.java
+++ src/main/java/com/marcinfriedrich/planningpoker/commons/DateTimeClock.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 import java.time.Instant;
 
 @Service
-public class DateTimeClock implements Clock {
+class DateTimeClock implements Clock {
     @Override
     public Instant getCurrentDate() {
         return Instant.now();
